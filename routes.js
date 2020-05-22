c8cfcdcfb0a64d746ec66a08de178edf801c3f6a
Alan Lee <alanleeone@gmail.com>
2020-05-21 20:20:53 -0700

    final commit moved constructor into routes

--- routes.js
+++ routes.js
@@ -1,30 +1,43 @@
 const router = require("express").Router();
-const { join } = require('path')
-const List = require("./List.js");
+const { join } = require("path");
+const { readFile, writeFile } = require('fs').promises
 
-let list = new List();
 
-router.get('/notes', (req, res) => {
-  res.sendFile(join(__dirname, 'public/notes.html'))
-})
+router.get("/notes", (req, res) => {
+  res.sendFile(join(__dirname, "public/notes.html"));
+});
 
 router.get("/api/notes", async (req, res) => {
-  let items = await list.getItems();
-  res.json(items);
+  // let items = await list.getItems();
+  res.json(JSON.parse(await readFile("./db/db.json")));
 });
 
 // POST an item (giving something to the server)
 router.post("/api/notes", async (req, res) => {
-  await list.addItem(req.body);
+  let item = req.body
+  let items = JSON.parse(await readFile("./db/db.json"));
+  item.id = items.length + 1;
+  items.push(item);
+  writeFile("./db/db.json", JSON.stringify(items));
+  // await list.addItem(req.body);
   res.sendStatus(200);
 });
-
-
 
 // DELETE an item (to delete something/data from the server)
-router.delete("/api/notes/:id", (req, res) => {
-  list.deleteItem(req.params.id);
+router.delete("/api/notes/:id", async (req, res) => {
+  let items = JSON.parse(await readFile("./db/db.json"));
+  items = items.filter((item) => {
+    if (item.id === parseInt(req.params.id)) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+  // console.log(items, id)
+  writeFile("./db/db.json", JSON.stringify(items));
+
+  // list.deleteItem(req.params.id);
   res.sendStatus(200);
 });
 
-module.exports = router
+module.exports = router;
